4dd50e5d2a8b6d47586146cc93d7874398d81354
Huda Yousif <hjdoski@gmail.com>
2019-10-09 21:06:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
-  array.join(" and ")
+  array.join(" and ")array[0...-1].join(", ") << ", and #{array[-1]}"
 end
 
- array[0...-1].join(", ") << ", and #{array[-1]}"
+ 
