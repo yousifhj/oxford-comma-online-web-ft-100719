24764faf60b447fc8b1d386a74c2904f144bd198
Huda Yousif <hjdoski@gmail.com>
2019-10-09 21:05:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,15 @@
 array = ["fiddleheads","okra","kohlrabi"]
 
+#def oxford_comma(array)
+ # array.join(" and ")
+#end
+
 def oxford_comma(array)
-  array.join(" and ")
-end
+when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end  
